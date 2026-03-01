53377a2ffff13c48d69f2c9f5263c2f7040e2de7
bolahuyy19-cyber <bolahuyy19@gmail.com>
2026-03-01 10:08:51 +0700

    Add files via upload

---
+++ package.js
@@ -0,0 +1,20 @@
+{
+  "name": "sunwin-sicbo-api",
+  "version": "1.0.0",
+  "description": "API lấy lịch sử và kết quả Sicbo Sunwin tự động cập nhật",
+  "main": "sicbosungoc.js",
+  "type": "commonjs",
+  "scripts": {
+    "start": "node sicbosungoc.js",
+    "dev": "nodemon sicbosungoc.js"
+  },
+  "author": "SangVu",
+  "license": "ISC",
+  "dependencies": {
+    "axios": "^1.6.0",
+    "express": "^4.18.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
+  }
+}
